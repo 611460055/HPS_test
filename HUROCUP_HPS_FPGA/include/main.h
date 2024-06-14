af7300909b22ff32dd19e875e06ca2331905fb9a
TSE0409 <osca0118@gmail.com>
2024-06-14 15:08:56 +0800

    建立git

---
+++ HUROCUP_HPS_FPGA/include/main.h
@@ -0,0 +1,12 @@
+
+/*
+ * main.h
+ *
+ *  Created on: 2019/09/11
+ *      Author: Yu-Chih, Wang
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#endif /* MAIN_H_ */
---
+++ HUROCUP_HPS_FPGA/main.c
@@ -0,0 +1,13 @@
+/*
+This program demonstrate how to use hps communicate with FPGA through light AXI Bridge.
+uses should program the FPGA by GHRD project before executing the program
+refer to user manual chapter 7 for details about the demo
+*/
+
+#include "include/main.h"
+ 
+
+int main()
+{	
+	return( 0 );
+}
